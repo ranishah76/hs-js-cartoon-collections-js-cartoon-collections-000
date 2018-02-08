c099d2e8484c5215a2c6f3ccb7c8762229bd4ea9
Rani Shah <rani76@gmail.com>
2018-02-08 11:01:09 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
 function dwarfRollCall(dwarves) {
-}
+  	var newStr = "";
+  	for(var i = 0; i < dwarves.length; i++) {
+  		if(i === dwarves.length-1) {
+  			newStr += (i+1 + ". " + dwarves[i]);
+  		}else {
+  			newStr += (i+1 + ". " + dwarves[i] + " ");
+  		}
+  	}
+  	return newStr;
+  }
+
 
 function summonCaptainPlanet(planeteerCalls){
+  return planeteerCalls.map(function(i) {
+		return i.toUpperCase() + "!";
+	});
 }
 
 function longPlaneteerCalls(words) {
+  for(var i = 0; i < words.length; i++) {
+  if(words[i].length > 4) {
+    return true;
+  }
+}
+return false;
 }
 
 function findTheCheese (foods) {
+  var cheese = ["camembert", "cheddar", "gouda"];
+for(var i = 0; i < foods.length; i++) {
+  var foundcheese = cheese.indexOf(foods[i]);
+  if(foundcheese!== -1) {
+    return foods[i];
+  }
+}
+return "no cheese!";
 }
